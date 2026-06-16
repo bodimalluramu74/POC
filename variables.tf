41f72cf4b7ac2271410783294353debb24e2c7e2
bodimalluramu74 <bodimalluramu74@gmail.com>
2026-06-16 15:50:57 +0530

    adding new code into variables

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+  
+}
+
+variable "ami" {
+    type = string
+    default = "ami-0521cb2d60cfbb1a6"
+}
+
+variable "count" {
+    type = number
+    default = 2
+  }
+
+variable "instance_types" {
+    type = list(string)
+    default = ["t3.micro","t3.small","t3.medium","t3.large"]
+  
+}
+
+variable "instance_tags" {
+    type = map(string)
+    default = {
+      "name" = "my-first_intance"
+      "Environment"= "DEV"
+    }
+  
+}
+
